840e135325386c0b5900f3f3a3770663da015f1b
Ahmed Ebrahim <eng.ahmed0302@gmail.com>
2025-06-17 06:08:09 +0300

    Test Rules

--- app/Http/Controllers/Api/ResponseTrait.php
+++ app/Http/Controllers/Api/ResponseTrait.php
@@ -2,7 +2,7 @@
 namespace App\Http\Controllers\Api;
 
 trait ResponseTrait{
-    public static function Response($data=null,$status,$msg=null){
+    public static function Response($data=null,$msg=null,$status){
         $array=[
             "data"=>$data,
             "msg"=>$msg,
